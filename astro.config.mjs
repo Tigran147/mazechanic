1eb500b88b09c2a05443e2c7ef0aeac6b006bd85
root <root@DESKTOP-CRN2M2R.localdomain>
2023-05-03 16:41:55 +0300

    work4

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://Tigran147.github.io',
-	base: '/',
+	base: '/mazechanic',
 	vite: {
 		ssr: {
 			external: ["svgo"]
