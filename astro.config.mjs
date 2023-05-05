d4dbcee29dd276239be9e1211b597fad5a9abb08
root <root@DESKTOP-CRN2M2R.localdomain>
2023-05-05 21:38:05 +0300

    everything commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://Tigran147.github.io',
+	site: 'https://',
 	base: '/mazechanic',
 	vite: {
 		ssr: {
